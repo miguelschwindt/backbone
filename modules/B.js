b3de04d0082d94ba367ed6dea088d9d9ca315543
AlfonsoAnaValentina <anavalentinaalfonso@gmail.com>
2014-09-18 11:12:58 -0300

    ejercicios 7 y 8 modificados

--- modules/B.js
+++ modules/B.js
@@ -1,21 +1,21 @@
 define(function(){
 
 	return{
-	 	v1='var1',
-		v2='var2',
-		v3='var3',
-	 	set=function(v1, v2, v3){
+	 	v1:'var1',
+		v2:'var2',
+		v3:'var3',
+	 	set:function(v1, v2, v3){
 			this.v1=v1;
 			this.v2=v2;
 			this.v3=v3;
 		}
-	 	getv1=function(){
+	 	getv1:function(){
 			return v1;
 		}
-	 	getv2=function(){
+	 	getv2:function(){
 			return v2;
 		}
-	 	getv3=function(){
+	 	getv3:function(){
 			return v3;
 		}
 	}
