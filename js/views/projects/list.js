72ab36c8bb0c59078d51801be6edf1721a159dde
AlfonsoAnaValentina <anavalentinaalfonso@gmail.com>
2014-09-24 11:47:31 -0300

    collection andando bien

--- js/views/projects/list.js
+++ js/views/projects/list.js
@@ -15,43 +15,36 @@ define([
   var ProjectListView = Backbone.View.extend({
     
     el: $("#container"),
-    
+    template: _.template(projectsListTemplate),
     initialize: function(){
       console.log('view.list');
-    //  var compiledTemplate = _.template( projectsListTemplate, { name: "Ginger Kid"} );
+   
       this.$el.html('<h1>Projects</h1>');
       this.projectsModels= new ProjectsModels; 
       this.projectsModels.set('propiedad1', "hola");
-      //console.log(this.projectsModels.get('propiedad1'));
+      
       this.projectsModels.set('propiedad2', "hola");
-      //console.log(this.projectsModels.get('propiedad2'));
-      this.render();    
-      },
-    render: function(){
-        var data =  [
+      
+      
+              var data =  [
                       PrimerModelo,
                       SegundoModelo
                           ];
 
       this.projectsCollection = new ProjectsCollection();
       this.projectsCollection.add(data);
-      var template = '<% _.each(this.projectsModels,function(model) { %><%= model.get("propiedad2") %><% }); %>';
-      var compiledTemplate = _.template(template);
-      console.log(this.projectsCollection.models);
+   
 
-      this.$el.html(compiledTemplate(this.projectsModels));
+      this.render();    
+      },
+    render: function(){
 
-       //this.projectsCollection.models));
-      //_.each(this.projectsCollection.models,function(model) { 
-        //   model.get("name");
-          // console.log(model.get("name"));
-         //});
 
-         //{ projects: this.projectsCollection.models } );
-     // this.$el.append(this.projectsCollection.model1.name);
+      this.$el.html(this.template(this.projectsCollection.models));
+
     }
   });
-  // Returning instantiated views can be quite useful for having "state"
+ 
   
   return ProjectListView;
 
