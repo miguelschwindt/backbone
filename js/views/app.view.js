a3968f65000ae213ec7b6babfefd88762ed5d5a4
Valentina Alfonso <anavalentinaalfonso@gmail.com>
2014-09-29 17:45:25 -0300

    ejercicio 3,4 temp con dudas

--- js/views/app.view.js
+++ js/views/app.view.js
@@ -11,13 +11,30 @@ define([
     el: $('#appDiv'),
 //$(divDeUser).style.visibility=hidden;
 //para que sea dinamico podria poner esta sentencia cuando salgo de la vista user
+//pero si uso hidden sigue atenta la seccion, si quiero que no este, usar collapse
 //y cuando entro poner esta otra
 //$(divDeUser).style.visibility=visible;
     render: function(){
       var data = {};
       var compiledTemplate = _.template( appTemplate, data );
       this.$el.append( compiledTemplate );
+    },
+    levantarVistaUser:function(){
+      this.userListView= new UserListView;
+      $('#userDiv').visibility.style=visible;
+    },
+    levantarVistaProject:function(){
+      this.projectListView= new ProjectListView;
+      $('#projectDiv').visibility.style=visible;
+    },
+    esconderVistaUser: function(){
+      $('#userDiv').visibility.style=collapse;
+    },
+    esconderVistaUser: function(){
+      $('#projectDiv').visibility.style=collapse;
     }
+
   });
+  
   return AppView;
 });
