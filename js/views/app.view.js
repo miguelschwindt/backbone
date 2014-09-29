52ac0363d260caadbf46e2c371bef71ff18220b9
Valentina Alfonso <anavalentinaalfonso@gmail.com>
2014-09-29 17:09:44 -0300

    ejercicio 3,4 temp con dudas

--- js/views/app.view.js
+++ js/views/app.view.js
@@ -1,19 +1,23 @@
-// Filename: views/project/list
+// Filename: views/app.view
 define([
   'jquery',
   'underscore',
   'backbone',
-  'text!templates/project/list.html'
-], function($, _, Backbone, projectListTemplate){
-  var ProjectListView = Backbone.View.extend({
-    el: $('#container'),
-    
-
+  'text!templates/appTemplate.html'
+  'views/projects/newlist'
+  'views/users/list'
+], function($, _, Backbone, appTemplate, ProjectListView, UserListView){
+  var AppView = Backbone.View.extend({
+    el: $('#appDiv'),
+//$(divDeUser).style.visibility=hidden;
+//para que sea dinamico podria poner esta sentencia cuando salgo de la vista user
+//y cuando entro poner esta otra
+//$(divDeUser).style.visibility=visible;
     render: function(){
       var data = {};
-      var compiledTemplate = _.template( projectListTemplate, data );
+      var compiledTemplate = _.template( appTemplate, data );
       this.$el.append( compiledTemplate );
     }
   });
-  return ProjectListView;
+  return AppView;
 });
